128c92bafeab1c2d6b3b56f43ac516c56b84a375
hugoochoa <hugo.ochoa.h@gmail.com>
2017-03-16 14:34:45 -0600

    examples/json_invoices/

---
+++ examples/json_invoices/json_invoices.php
@@ -0,0 +1,134 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+try {
+
+    /*
+    IMPORTANTE: remplaza el valor de tu API key y un ID de usuario de Sync donde se indica:
+    */
+    $api_key = 'YOUR_API_KEY';
+    $id_user = 'SOME_SYNC_ID_USER';
+
+    /*
+    Inicializa algunas variables globales que se utilizarán:
+    */
+
+    $SAT_SITE_ORGANIZATION = '56cf4ff5784806152c8b4568';
+    $XML_ATTACHMENT_TYPE = '56bcdfca784806d1378b4567';
+    $FILE_NAME = 'facturas.json';
+
+    /*
+    Inicializa el SDK con tu API key de Sync:
+    */
+
+    $env = 'https://syncdev.paybook.com/v1/';
+    paybook\Paybook::init($api_key, null, null, $env);
+    // paybook\Paybook::init($api_key);
+
+    /*
+    Crea el usuario con el que se descargarán las facturas (únicamente instancía un usuario ya existente):
+    */
+    $user = new paybook\User(null, $id_user);
+
+    /*
+    Crea una sesión para el usuario seleccionado:
+    */
+    $session = new paybook\Session($user);
+
+    /*
+    Obtiene las transacciones del SAT en un periodo específico:
+    */
+
+    $options = [
+        'id_site_organization' => $SAT_SITE_ORGANIZATION,
+        'limit' => 5,
+    ];
+
+    $transactions = paybook\Transaction::get($session, null, $options);
+
+    /*
+    Si no hay transacciones del SAT termina la ejecución
+    */
+
+    if (count($transactions) == 0) {
+        echo PHP_EOL;
+        exit(' Tu usuario de Sync no cuenta con transacciones sincronizadas del SAT'.PHP_EOL.PHP_EOL);
+    }
+
+    /*
+    Si hay transacciones del SAT itera sobre ellas y obtiene los XML parseados:
+    */
+
+    $xmls_parsed = [];
+    $xmls_obtained = 0;
+
+    echo PHP_EOL;
+
+    foreach ($transactions as $i => $transaction) {
+
+        /*
+        Por cada transaccione del SAT checa si tiene archivos adjuntos (si no tiene se va a la sig. transacción)
+        */
+        if (is_null($transaction->attachments)) {
+            continue;
+        }
+
+        /*
+        Si la transacción tiene archivos adjuntos checa cuál de estos es XML
+        */
+        foreach ($transaction->attachments as $j => $attachment) {
+
+            /*
+            Si el archivo no es XML continua con el sig. archivo adjunto ligado a esta transacción del SAT
+            */
+            if ($attachment['id_attachment_type'] != $XML_ATTACHMENT_TYPE) {
+                continue;
+            }
+
+            /*
+            Si el archivo adjunto ex un XMl obtiene su versión parseada a JSON
+            */
+
+            $id_attachment = $attachment['id_attachment'];
+
+            $xml_parsed = paybook\Attachment::get($session, null, $id_attachment, $extra = true);
+
+            $xmls_obtained = $xmls_obtained + 1;
+
+            echo '   '.strval($xmls_obtained).'. '.$attachment['file'].' obtenido'.PHP_EOL;
+            array_push($xmls_parsed, $xml_parsed);
+        }
+    }
+
+    if (count($xmls_parsed) == 0) {
+        echo PHP_EOL;
+        exit(' Las transacciones del SAT no cuenta con XMLs adjuntos'.PHP_EOL.PHP_EOL);
+    }//End of if
+
+    $json = [
+        'facturas' => $xmls_parsed,
+    ];
+    $fp = fopen($FILE_NAME, 'w');
+    fwrite($fp, json_encode($json, JSON_PRETTY_PRINT));
+    fclose($fp);
+
+    echo PHP_EOL;
+    echo PHP_EOL;
+    echo ' Las facturas fueron descargadas en formato JSON y guardadas en el archivo: '.PHP_EOL;
+    echo PHP_EOL;
+    echo '   '.__DIR__.'/'.$FILE_NAME.PHP_EOL;
+    echo PHP_EOL;
+    echo ' \0/ Script ejecutado exitósamente. Gracias por usar Sync'.PHP_EOL;
+    echo PHP_EOL;
+} catch (paybook\Error $e) {
+    echo PHP_EOL;
+    echo ' ¡Ups! Ocurrió un error, respuesta de Sync API:'.PHP_EOL;
+    echo PHP_EOL;
+    echo '   '.$e->get_code().' '.$e->get_message().PHP_EOL;
+    echo PHP_EOL;
+    echo ' Puedes consultar la lista de errores del Sync API en la siguiente liga:'.PHP_EOL;
+    echo PHP_EOL;
+    echo '   https://www.paybook.com/sync/docs/API#es&response'.PHP_EOL;
+    echo PHP_EOL;
+}//End of try
---
+++ examples/json_invoices/readme.md
@@ -0,0 +1,65 @@
+     
+# Descarga de Facturas en formato JSON
+
+Este es un ejemplo de cómo descargar las facturas que Sync sincroniza en formato JSON. 
+
+### Requerimientos
+
+- [PHP](http://php.net/)
+- [Instalar PHP Composer](https://getcomposer.org/doc/00-intro.md)
+- Una API key de Sync
+- Una usario de Sync con transacciones del SAT (facturas) previamente sincronizadas.
+
+### Ejecución
+
+Abrir tu CLI. 
+
+Clonar este repositorio en tu computadora:
+	
+	git clone https://github.com/Paybook/sync-php.git
+
+Moverse al directorio de este ejemplo:
+
+	cd sync-php/examples/json_invoices
+
+Instalar la biblioteca de Paybook:
+
+	composer install
+
+Modifica los valores de las variables API key y ID user dentro del script:
+	
+	$api_key = 'YOUR_API_KEY';
+    $id_user = 'SOME_SYNC_ID_USER';
+
+Una vez hecho esto ejecuta el script:
+	
+	php json_invoices.php
+
+Observarás que en tu CLI lo siguiente:
+	
+	1. 3992153FCC6C4640AAA289FD8F68D700.xml obtenido
+   	2. 38B34374E0724756B796A98C10E76B38.xml obtenido
+   	3. 0C6213C4F8004D72AEF118BF24DD5FDC.xml obtenido
+   	4. F3BB2522AA7D494FADFB13188CC7D021.xml obtenido
+   	5. 271A1587FE584960BC7A3F81C3DAD8C6.xml obtenido
+
+
+ 	Las facturas fueron descargadas en formato JSON y guardadas en el archivo: 
+
+   	/Users/hugo/Documents/paybook/dev/libraries/sync-php/examples/attachments_extra/facturas.json
+
+ 	\0/ Script ejecutado exitósamente. Gracias por usar Sync
+
+Puedes observar que las facturas descargadas en formato JSON fueron guardadas en un archvo en este mismo directorio:
+
+	facturas.json
+	
+Este script descaga únicamente 5 facturas. Sin embargo, puedes modificarlo para descargar el número de facturas que desees.
+
+
+
+
+
+
+
+
